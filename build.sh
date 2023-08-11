b4183708540bb39095e599fccf3eaaa0e129f72f
James Martin <james.martin@bugsnag.com>
2023-08-11 13:24:43 +0100

    Optimise Dockerfile

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ echo "---> [Building app image]"
 docker $BUILDX build $FLAGSX -t ghcr.io/zombar/traces-demo:latest -f - . <<EOF
 FROM python:3.11
 
-COPY ./ /app/
+COPY ./Pipfile* /app/
 
 WORKDIR /app
 
@@ -10,5 +10,6 @@ RUN pip install pipenv
 RUN pipenv install --system --deploy
 RUN pip install "uvicorn[standard]"
 
+COPY ./*.py /app/
 ENTRYPOINT ["python", "utils.py"]
 EOF
